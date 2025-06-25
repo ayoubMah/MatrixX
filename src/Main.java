7b28ec78e2cf9877c77cfe37018c35296a043725
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-06-25 18:41:51 +0200

    Can you now generate your random nxm matrix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        double [][] data = {{1,1,1},{1,1,1},{1,1,1}};
-        int rows = data.length;
-        int cols = data[0].length;
-        Matrix matrix = new Matrix(rows, cols );
+
+        System.out.println("================================================");
+
+        MatrixUtils matrixUtils = new MatrixUtils(4 , 5);
+        int[][] randomeMatrix = matrixUtils.rendomMatrix();
+
+        for (int i = 0; i < matrixUtils.getRows(); i++) {
+            for (int j = 0; j < matrixUtils.getCols(); j++) {
+                System.out.print(randomeMatrix[i][j] + "   ");
+            }
+            System.out.println();
+        }
 
 
     }
--- src/MatrixUtils.java
+++ src/MatrixUtils.java
@@ -1,2 +1,37 @@
 public class MatrixUtils {
+
+    private int rows;
+    private int cols;
+
+    public MatrixUtils(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public void setCols(int cols) {
+        this.cols = cols;
+    }
+
+    // generate random matrix nxm =>  0<=n,m <=10
+    public int[][] rendomMatrix() {
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = (int)(Math.random()*10);
+            }
+        }
+        return matrix;
+    }
 }
